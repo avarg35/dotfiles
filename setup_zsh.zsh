e37616140ae4f1f30a2a2b27c20473c80d1caf86
Andrew Vargas <79120637+avarg35@users.noreply.github.com>
2021-05-30 15:19:07 -0400

    Fixed syntax issues in "if" statements

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -8,16 +8,16 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
   echo '/usr/local/bin/zsh already exists in /etc/shells'
 else
-# echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+  # echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
- 
 
-if ["$SHELL" = '/usr/local/bin/zsh']; then
+
+if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
   echo '$SHELL is already /usr/local/bin/zsh'
 else
-echo "Enter user password to change login shell"
-chsh -s '/usr/local/bin/zsh'
+  echo "Enter user password to change login shell"
+  chsh -s '/usr/local/bin/zsh'
 fi
 
 
